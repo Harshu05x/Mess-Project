661fc5f609b300f5b8424c78e08e35795dc816e2
Harsh05x <harshumadhbhave25@gmail.com>
2024-07-26 12:50:36 +0530

    Initial Commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Sample Shadcn Template</h1>
+      <Button className="mt-4">Click me</Button>
+    </main>
+  )
+}
